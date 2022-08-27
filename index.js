420d93bca4ff2077a95a8452517354a82a725269
Gentrit <gentritrexhaa@gmail.com>
2022-08-27 13:26:52 +0200

    mySite_v1.3
    Images are made responsive with bootstrap.
    Z-index is added
    All dimensions are relative

--- index.js
+++ index.js
@@ -1,42 +1,46 @@
- document.getElementsByClassName('cloud3')[0].style.right="200px";
- document.getElementsByClassName('cloud2')[0].style.left="0px";
- document.getElementsByClassName('cloud1')[0].style.left="330px";
- document.getElementsByClassName('cloud1')[0].style.top="100px";
- document.getElementsByClassName('parachute')[0].style.top="50px";
- document.getElementsByClassName('parachute')[0].style.right="0px";
+document.getElementsByClassName('cloud1')[0].style.left = "40%";
+document.getElementsByClassName('cloud1')[0].style.top = "13%";
+document.getElementsByClassName('cloud2')[0].style.left = "0px";
+document.getElementsByClassName('cloud2')[0].style.top = "18%";
+document.getElementsByClassName('cloud3')[0].style.right = "13%";
+document.getElementsByClassName('cloud3')[0].style.top = "30%";
+document.getElementsByClassName('parachute')[0].style.top = "-100px";
+document.getElementsByClassName('parachute')[0].style.right = "0%";
 
- let i = 0;
- let xPositionParachute;
- let yPositionParachute;
- let xPositionCloud1;
- let yPositionCloud1;                  
- let positionCloud2;
- let positionCloud3; 
+let i = 0;
+let xPositionParachute;
+let yPositionParachute;
+let xPositionCloud1;
+let yPositionCloud1;
+let positionCloud2;
+let positionCloud3;
 
-function myLoop() { 
-    xPositionParachute = parseFloat(document.getElementsByClassName('parachute')[0].style.right);
-    yPositionParachute = parseFloat(document.getElementsByClassName('parachute')[0].style.top);
-
+function myLoop() {
+   
     xPositionCloud1 = parseFloat(document.getElementsByClassName('cloud1')[0].style.left);
     yPositionCloud1 = parseFloat(document.getElementsByClassName('cloud1')[0].style.top);
     positionCloud2 = parseFloat(document.getElementsByClassName('cloud2')[0].style.left);
-    positionCloud3 = parseFloat(document.getElementsByClassName('cloud3')[0].style.right);        
-    
+    positionCloud3 = parseFloat(document.getElementsByClassName('cloud3')[0].style.right);
+    xPositionParachute = parseFloat(document.getElementsByClassName('parachute')[0].style.right);
+    yPositionParachute = parseFloat(document.getElementsByClassName('parachute')[0].style.top);
+
     setTimeout(
-        ()=> {   
-                document.getElementsByClassName('parachute')[0].style.right=`${xPositionParachute+2}px`;
-                document.getElementsByClassName('parachute')[0].style.top=`${yPositionParachute+ 1}px`;
-            
-                document.getElementsByClassName('cloud1')[0].style.left=`${xPositionCloud1+0.5}px`;
-                document.getElementsByClassName('cloud1')[0].style.top=`${yPositionCloud1- 0.5}px`;
-                document.getElementsByClassName('cloud2')[0].style.left=`${positionCloud2+1.5}px`;
-                document.getElementsByClassName('cloud3')[0].style.right=`${positionCloud3+2}px`;
-                i++;           
-            if(i<400){  
-                 myLoop();             
-                    }
+        () => {
+            document.getElementsByClassName('parachute')[0].style.right = `${xPositionParachute + 2}px`;
+            document.getElementsByClassName('parachute')[0].style.top = `${yPositionParachute + 1}px`;
+
+            document.getElementsByClassName('cloud1')[0].style.left = `${xPositionCloud1 + 0.5}px`;
+            document.getElementsByClassName('cloud1')[0].style.top = `${yPositionCloud1 - 0.5}px`;
+            document.getElementsByClassName('cloud2')[0].style.left = `${positionCloud2 + 1.5}px`;
+            document.getElementsByClassName('cloud3')[0].style.right = `${positionCloud3 + 2}px`;
+            i++;
+            if (i < 1000) {
+                myLoop();
             }
-    , 100);
-    }
+        }
+        , 100);
+}
 
 myLoop();
+
+
